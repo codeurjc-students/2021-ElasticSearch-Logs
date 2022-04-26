e1655c93832e5ee113bc57dc0c25ac49eb2b1e5d
cfres <cris.dgrnu@gmail.com>
2022-04-26 10:45:28 +0200

    Add test for search

--- backend/src/test/java/com/elasticsearchlogs/elasticsearchlogsbackend/e2e/IndexTest.java
+++ backend/src/test/java/com/elasticsearchlogs/elasticsearchlogsbackend/e2e/IndexTest.java
@@ -1,11 +1,16 @@
 package com.elasticsearchlogs.elasticsearchlogsbackend.e2e;
 
 
+import com.elasticsearchlogs.elasticsearchlogsbackend.index.IndexRestController;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
@@ -28,7 +33,7 @@ public class IndexTest {
         return loginRes.getCookie("AuthToken");
     }
     @Test
-    public void getAllIndices(){
+    public void get_all_indices(){
 
         given()
                 .cookie("AuthToken",this.getAuthCookie())
---
+++ backend/src/test/java/com/elasticsearchlogs/elasticsearchlogsbackend/e2e/SearchTest.java
@@ -0,0 +1,81 @@
+package com.elasticsearchlogs.elasticsearchlogsbackend.e2e;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class SearchTest {
+
+    private String getAuthCookie(){
+        RequestSpecification loginReq = RestAssured.given();
+
+        JSONObject loginReqParams = new JSONObject();
+        loginReqParams.put("username", "elasticadmin");
+        loginReqParams.put("password", "MY_SECRET");
+
+        loginReq.header("Content-Type", "application/json");
+        loginReq.body(loginReqParams.toJSONString());
+
+        Response loginRes = loginReq.post("http://localhost:8080/api/auth/login");
+
+        return loginRes.getCookie("AuthToken");
+    }
+
+    @Test
+    public void post_match_search(){
+        JSONObject body = new JSONObject();
+        body.put("fields", List.of("log_level"));
+        body.put("searchTerms", List.of("INFO"));
+
+        given()
+                .cookie("AuthToken",this.getAuthCookie())
+                .header("Content-Type", "application/json")
+                .body(body.toJSONString())
+        .when()
+                .post("http://localhost:8080/api/log/match-search")
+        .then()
+                .statusCode(200)
+                .body("size()",is(200));
+    }
+
+    @Test
+    public void post_wildcard_search(){
+        JSONObject body = new JSONObject();
+        body.put("fields", List.of("log_level"));
+        body.put("searchTerms", List.of("INFO"));
+
+        given()
+                .cookie("AuthToken",this.getAuthCookie())
+                .header("Content-Type", "application/json")
+                .body(body.toJSONString())
+        .when()
+                .post("http://localhost:8080/api/log/wildcard-search")
+        .then()
+                .statusCode(200)
+                .body("size()",is(200));
+    }
+
+    @Test
+    public void post_range_search(){
+        JSONObject body = new JSONObject();
+        body.put("fields", List.of("@timestamp"));
+        body.put("searchTerms", List.of("2022-04-24T00:00:00.000+00:00","2022-04-24T04:00:00.000+00:00,"));
+
+        given()
+                .cookie("AuthToken",this.getAuthCookie())
+                .header("Content-Type", "application/json")
+                .body(body.toJSONString())
+        .when()
+                .post("http://localhost:8080/api/log/range-search")
+        .then()
+                .statusCode(200);
+    }
+}
