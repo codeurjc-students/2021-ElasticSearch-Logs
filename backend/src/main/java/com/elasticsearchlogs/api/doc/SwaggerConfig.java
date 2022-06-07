4d18579991b5b8ad8235325330c0cf3417e1ee02
CrisDgrnu <cris.dgrnu@gmail.com>
2022-06-07 19:41:19 +0200

    Swagger hotfix

--- backend/src/main/java/com/elasticsearchlogs/api/doc/SwaggerConfig.java
+++ backend/src/main/java/com/elasticsearchlogs/api/doc/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.elasticsearchlogs.elasticsearchlogsbackend"))
+                .apis(RequestHandlerSelectors.basePackage("com.elasticsearchlogs.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
